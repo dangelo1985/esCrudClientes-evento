189d2937d23560ae9a547571dbc6f7ea8d352178
Rafael DAngelo <dangelo_1985@hotmail.com>
2023-01-07 11:24:42 -0300

    Project Create

---
+++ backend/src/main/java/com/eventospring/esCrudClientes/EsCrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.eventospring.esCrudClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsCrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsCrudClientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eventospring/esCrudClientes/EsCrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventospring.esCrudClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsCrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
