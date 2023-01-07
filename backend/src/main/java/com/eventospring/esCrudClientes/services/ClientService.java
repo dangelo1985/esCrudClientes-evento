fac72db0df7f9f98dbb764a14d25f23bcb3402b0
Rafael DAngelo <dangelo_1985@hotmail.com>
2023-01-07 15:51:31 -0300

    Database seeding

--- backend/src/main/java/com/eventospring/esCrudClientes/services/ClientService.java
+++ backend/src/main/java/com/eventospring/esCrudClientes/services/ClientService.java
@@ -2,8 +2,11 @@ package com.eventospring.esCrudClientes.services;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eventospring.esCrudClientes.entities.Client;
 import com.eventospring.esCrudClientes.repositories.ClientRepository;
@@ -14,6 +17,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Client> findAll(){
 		
 		return repository.findAll();
